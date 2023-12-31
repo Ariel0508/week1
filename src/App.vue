4bc3a7095a4be08b7272e8e0069bdeac4c5ec854
Ariel <sonyt605@gmail.com>
2023-12-31 16:38:25 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script>
+import ListComponent from './components/ListComponent.vue'
+export default {
+  data() {
+    return {
+      tempProduct: {}
+    }
+  },
+  components:{
+    ListComponent,
+  }
+}
+</script>
+
+<template>
+  <div class="container">  
+        <ListComponent />
+  </div>
+</template>
+
+<style lang="scss">
+@import './assets/all.scss';
+    img {
+      object-fit: contain;
+      max-width: 100%;
+    }
+
+    .primary-image {
+      height: 300px;
+    }
+
+    .images {
+      height: 150px;
+    }
+</style>
